7c142e52dbd0ded5d4a34760946891a77065e279
Luis Parente <lgsp25@hotmail.com>
2025-07-27 07:15:11 -0300

    projeto criado

---
+++ src/main/java/com/example/voll/medica/ClinicaMedicaVoll/ClinicaMedicaVollApplication.java
@@ -0,0 +1,13 @@
+package com.example.voll.medica.ClinicaMedicaVoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaVollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaVollApplication.class, args);
+	}
+
+}
